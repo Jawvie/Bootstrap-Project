378a91301466ea45709651d3b2bc14313281b05e
Javier Perez <jawvie3@gmail.com>
2022-10-04 17:25:54 -0700

    NPM scripts pt1.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// JQUERY FUNCTION BELOW FOR PLAY/PAUSE CAROUSEL
+
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );                          //INTERVAL/TIMER
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {      //checking if pause button
+            $(".carousel").carousel("pause");                               //pause command?
+            $("#carouselButton").children("i").removeClass("fa-pause");     //removing font awesome pause
+            $("#carouselButton").children("i").addClass("fa-play");         //adding fron awesome play
+        } else {
+            $(".carousel").carousel("cycle");                               //else it is a play button start "cycle"
+            $("#carouselButton").children("i").removeClass("fa-play");      //remove fa play add pause
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+});
